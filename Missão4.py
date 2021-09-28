14a783892a8d8ce03182263dc73a592ca28f4870
cefastdrone <62629740+cefastdrone@users.noreply.github.com>
2021-09-28 13:53:26 -0300

    Add files via upload

---
+++ Missão4.py
@@ -0,0 +1,48 @@
+def takeoff():
+    """
+    Inicializa o drone e o mantem no lugar até receber o próximo comando
+    """
+
+
+def hover():
+    """
+    cancela o último comando e fica no lugar parado
+    """
+
+
+def land():
+    """
+    Pousa o drone
+    """
+
+
+def move(x, y, z):
+    """
+    O drone se move até a localidade marcada como sendo x, y, z
+    :param x:
+    :param y:
+    :param z:
+    :return:
+    """
+
+
+def turn_by(x, y, z):
+    """
+    Muda a direção do drone de acordo com os parâmetros
+    :param x: roll
+    :param y: pitch
+    :param z: yaw
+    """
+
+
+def get_postion():
+    """
+    :return: posição do drone em relação ao ponto on ele começou como um dicionário
+    """
+
+
+def get_orientation():
+    """
+    :return: Orientação do drone em relação a posição em que ele começou como um dicionário
+    'roll':  , 'pitch':  , 'yaw':
+    """
